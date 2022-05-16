4563fe4d766bf0fe0a10ab855bdc07cb7ba8dcf7
yaroslav.koval <yaroslav@koval.page>
2022-05-16 15:29:56 +0200

    load moderators ids fix (change to map)

--- static/config.py
+++ static/config.py
@@ -2,5 +2,5 @@ from os import getenv
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_OWNER = getenv("BOT_OWNER")
-MODERATORS = [int(i) for i in getenv("MODERATORS").split()]
+MODERATORS = list(map(int, getenv("MODERATORS").split()))
 DATABASE_URL = getenv("DATABASE_URL")
