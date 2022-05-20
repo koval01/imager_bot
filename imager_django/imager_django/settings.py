ce5b135fc8905058eb7a75a9bcbe018a78a89dbc
yaroslav.koval <yaroslav@koval.page>
2022-05-20 20:14:47 +0200

    rebuild with git

--- imager_django/imager_django/settings.py
+++ imager_django/imager_django/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-<7f72oq45i5s:tv{7fvl}m<dhn|kr8n2??c4j|u:f1>6020ym}a0g03=8ct'
+SECRET_KEY = 'django-insecure-<7f72oq45i5s:tv{7fvl}m<67dhn|kr8n2??c4j|9u:f1>6020ym}a0g03=8ct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
