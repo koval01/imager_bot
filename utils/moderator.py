210dd73d45a6d7884c4d245e339525be2f5011ef
yaroslav.koval <yaroslav@koval.page>
2022-05-25 01:05:35 +0200

    check and update user name (add in middleware)

--- utils/moderator.py
+++ utils/moderator.py
@@ -1,6 +1,8 @@
 from aiogram import types
 from database.controller import session_factory
 from database.models import Moderator
+from content.manager import Manager
+import logging as log
 
 
 class CheckModerator:
@@ -10,6 +12,8 @@ class CheckModerator:
 
     @property
     def _check_user(self) -> bool:
+        user_check = Manager(message=self.message).check_user
+        log.debug("Error check user.") if not user_check else None
         x = self.session.query(Moderator).filter_by(
             tg_user_id=self.message.from_user.id)
         return True if x.count() else False
