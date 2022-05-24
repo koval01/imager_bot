16f38ed5b1963ccabc9deef34b249b6d58ba50bf
yaroslav.koval <yaroslav@koval.page>
2022-05-24 16:58:31 +0200

    update regex pattern for redis link

--- static/config.py
+++ static/config.py
@@ -1,7 +1,7 @@
 from os import getenv
 import re
 
-pattern_redis = re.compile(r"redis:\/\/:(?P<password>[a-z0-9\+\\]*?)@(?P<host>[a-z0-9\-\.]*?):(?P<port>[0-9]*$)")
+pattern_redis = re.compile(r"redis:\/\/:(?P<password>[A-z0-9]*?)@(?P<host>[a-z0-9\-\.]*?):(?P<port>[0-9]*$)")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_OWNER = int(getenv("BOT_OWNER"))
