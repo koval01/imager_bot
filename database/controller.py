db62af73dc8734116fe557fdafcf837ff8d0089f
yaroslav.koval <yaroslav@koval.page>
2022-05-30 22:23:26 +0200

    sqlalchemy configuration update

--- database/controller.py
+++ database/controller.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine(config.DATABASE_URL)
+engine = create_engine(config.DATABASE_URL, pool_size=20, max_overflow=40)
 _SessionFactory = sessionmaker(bind=engine)
 
 Base = declarative_base()
