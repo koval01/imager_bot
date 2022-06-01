90c22056d7af1656af8a48707ee9553bdc5062f9
yaroslav.koval <yaroslav@koval.page>
2022-06-01 21:33:27 +0200

    CSRF fix (Django)

--- imager_django/imager_django/settings.py
+++ imager_django/imager_django/settings.py
@@ -120,6 +120,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
