79c61b131f45ea1c3e9e603be4c8577fde0340d2
yaroslav.koval <yaroslav@koval.page>
2022-06-02 20:51:55 +0200

    update error handlers (Django)

--- imager_django/imager/views.py
+++ imager_django/imager/views.py
@@ -16,19 +16,3 @@ def get_image(request, file_type, file_name) -> StreamingHttpResponse:
     if file_type == "error":
         return error_load(code=int(file_name[:3]))
     return TelegramAPI(path=f"{file_type}/{file_name}").get_file
-
-
-def error_response(request, exception='Unknown', status_code=500) -> JsonResponse:
-    return JsonResponse(
-        {
-            "status_code": status_code, "stamp": hex(int(time() * 1000000)),
-            "exception": exception
-        }, status=status_code
-    )
-
-error_400 = lambda r, e: error_response(r, e, 400)
-error_401 = lambda r, e: error_response(r, e, 401)
-error_403 = lambda r, e: error_response(r, e, 403)
-error_404 = lambda r, e: error_response(r, e, 404)
-error_405 = lambda r, e: error_response(r, e, 405)
-error_500 = lambda r, e: error_response(r, e, 500)
--- imager_django/imager_django/urls.py
+++ imager_django/imager_django/urls.py
@@ -1,18 +1,3 @@
-"""imager_django URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,10 +5,3 @@ urlpatterns = [
     path('app/', include('imager.urls')),
     path('', admin.site.urls),
 ]
-
-handler400 = 'imager.views.error_400'
-handler401 = 'imager.views.error_401'
-handler403 = 'imager.views.error_403'
-handler404 = 'imager.views.error_404'
-handler405 = 'imager.views.error_405'
-handler500 = 'imager.views.error_500'
