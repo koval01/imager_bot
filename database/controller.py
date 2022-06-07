d4427278de3129d0b087390a93881291851cabb7
yaroslav.koval <yaroslav@koval.page>
2022-06-07 19:45:53 +0200

    experimental update (test SingletonThreadPool for sqlalchemy)

--- database/controller.py
+++ database/controller.py
@@ -2,11 +2,11 @@ from static import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
+from sqlalchemy.pool import SingletonThreadPool
 
 
 engine = create_engine(
-    config.DATABASE_URL, poolclass=StaticPool
+    config.DATABASE_URL, poolclass=SingletonThreadPool, pool_size=12
 )
 _SessionFactory = sessionmaker(bind=engine)
 
