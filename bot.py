9ef6b4d3747d01ac3c71470b5e1ea11ff058c834
yaroslav.koval <yaroslav@koval.page>
2022-06-08 13:41:49 +0200

    integrate sentry to bot

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 from aiogram import executor
 from dispatcher import dp
 import handlers
+import sentry_sdk
+import os
+
+sentry_sdk.init(
+    os.getenv("SENTRY_BOT"),
+    traces_sample_rate=1.0
+)
 
 
 async def shutdown_actions(dp) -> None:
