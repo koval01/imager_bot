d0c6b933ef0db3028704a2b3e4dc2da913937047
yaroslav.koval <yaroslav@koval.page>
2022-06-11 21:21:48 +0200

    throttling limits up

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -35,7 +35,7 @@ async def cancel_action(msg: types.Message, state: FSMContext):
 
 
 @dp.message_handler(commands=['start', 'help'], state="*")
-@rate_limit(1.5, 'start_command')
+@rate_limit(2, 'start_command')
 async def send_welcome(msg: types.Message, state: FSMContext):
     await msg.reply(dict_reply["start_message"], reply_markup=build_menu("start_menu"))
     await state.finish()
@@ -120,7 +120,7 @@ async def wait_content_handler(msg: types.Message):
     if response_status != dict_reply["content_success"]:
         return await msg.reply(response_status)
     try:
-        await dp.throttle('start', rate=1)
+        await dp.throttle('start', rate=1.3)
     except Throttled:
         pass
     else:
@@ -138,26 +138,26 @@ async def wait_content_handler_invalid_type(msg: types.Message):
 
 
 @dp.message_handler(lambda message: message.text not in dict_menu["select_mode"], state=ViewContent.select_mode)
-@rate_limit(0.8, 'error_select_content_type')
+@rate_limit(1, 'error_select_content_type')
 async def invalid_select_content(msg: types.Message):
     await msg.reply(dict_reply["error_select"], reply_markup=build_menu("select_mode"))
 
 
 @dp.message_handler(lambda message: message.text not in dict_menu["next_content"], state=ViewContent.view_mode)
-@rate_limit(0.8, 'error_select_content_action')
+@rate_limit(1, 'error_select_content_action')
 async def invalid_select_action(msg: types.Message):
     await msg.reply(dict_reply["error_action"], reply_markup=build_menu("next_content"))
 
 
 @dp.message_handler(lambda message: message.text not in dict_menu["rand_or_last"],
                     state=ViewContent.select_mode_stage_two)
-@rate_limit(0.8, 'error_select_get_mode')
+@rate_limit(1, 'error_select_get_mode')
 async def invalid_select_get_mode(msg: types.Message):
     await msg.reply(dict_reply["rand_or_last_error_select"], reply_markup=build_menu("rand_or_last"))
 
 
 @dp.message_handler(lambda message: message.text == dict_menu["next_content"][1], state=ViewContent.view_mode)
-@rate_limit(0.6, 'next_content')
+@rate_limit(0.7, 'next_content')
 async def next_action(msg: types.Message, state: FSMContext):
     async with state.proxy() as data:
         msg.text = data["select"]
