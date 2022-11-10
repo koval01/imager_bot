82f43355c3da02771b3ff0f3df7a01bb45fdf8a5
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-11-10 16:23:25 +0200

    fix redis link pattern

--- static/config.py
+++ static/config.py
@@ -1,7 +1,7 @@
 import re
 from os import getenv
 
-pattern_redis = re.compile(r"redis:\/\/:(?P<password>[A-z0-9]*?)@(?P<host>[a-z0-9\-\.]*?):(?P<port>[0-9]*$)")
+pattern_redis = re.compile(r"rediss:\/\/:(?P<password>[A-z0-9]*?)@(?P<host>[a-z0-9\-\.]*?):(?P<port>[0-9]*$)")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_OWNER = int(getenv("BOT_OWNER"))
